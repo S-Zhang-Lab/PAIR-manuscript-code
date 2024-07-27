d52196d40e23b212ecfb0d7d3b52f1c70786fb8d
Siyuan Zhang <Siyuan.Zhang@UTSouthwestern.edu>
2024-07-27 11:49:59 -0500

    updated the oligo assemble code

--- ddr-screen/Codes/Lib_assemble.R
+++ ddr-screen/Codes/Lib_assemble.R
@@ -35,11 +35,33 @@ combinations <- combinations %>%
          CasRx = CasRx_sequences$Sequence[CasRx_index],
          CRISPRa_name = CRISPRa_sequences$name[CRISPRa_index],
          CasRx_name = CasRx_sequences$name[CasRx_index])
+head(combinations)
 
 # Extract the gene names from the "name" column for comparison
 combinations <- combinations %>%
   mutate(CRISPRa_ID = sub("_CRISPRa.*", "", CRISPRa_name),
          CasRx_ID = sub("_CasRx.*", "", CasRx_name))
+head(combinations)
 
 # Filter out combinations where the sequences are from the same gene name (ID)
 combinations <- combinations %>% filter(CRISPRa_ID != CasRx_ID)
+head(combinations)
+
+# Create the final sequences
+combinations <- combinations %>%
+  mutate(Final_oligos = paste0(e1, CRISPRa, e3, CasRx, e5))
+
+# Create the final table
+Final <- data.frame(seq = seq(1:nrow(combinations)), combinations)
+
+# Rename the sequence column to "seq_ID"
+colnames(Final)[1] <- "seq_ID"
+
+# Display the final data frame
+print(head(Final))
+dim(Final)
+
+# Save table
+write.table(Final, file = "./Misc/Final_oligos.csv", sep = "\t", row.names = FALSE, col.names = TRUE)
+
+
