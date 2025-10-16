8c6a9292402014ffe42403b8fd2bc73a07d1cf04
RavenGan <dailin.raven.gan@gmail.com>
2025-10-16 17:08:24 -0400

    pair extraction using step 1 scripts

---
+++ perturb-seq/code/PAIR_extraction/Step1_PAIR_extraction.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+#$ -M dgan@nd.edu
+#$ -m abe               # Send mail when job begins, ends and aborts
+#$ -q long              # Specify queue
+#$ -pe smp 4            # Specify number of cores to use.
+#$ -N PAIR_extraction   # Specify job name
+
+module load python
+source /afs/crc.nd.edu/group/StatDataMine/dm008/Dailin_Gan/Siyuan/PAIR-perturb-seq_2025-10/venv/bin/activate
+
+DATA_DIR=/afs/crc.nd.edu/group/StatDataMine/dm008/Dailin_Gan/Siyuan/PAIR-perturb-seq_2025-10/raw_data/01.RawData/PAIR_TT_C12
+OUT_DIR=/afs/crc.nd.edu/group/StatDataMine/dm008/Dailin_Gan/Siyuan/PAIR-perturb-seq_2025-10/PAIR_output
+
+# full run
+python 01_Extract_PAIR.py   --input_r1 ${DATA_DIR}/PAIR_TT_C12-SI_TT_C12_2333FLLT3_S3_L007_R1_001.fastq.gz \
+                            --input_r2 ${DATA_DIR}/PAIR_TT_C12-SI_TT_C12_2333FLLT3_S3_L007_R2_001.fastq.gz \
+                            --output_r1 ${OUT_DIR}/PAIR_matched_R1.fastq.gz \
+                            --output_r2 ${OUT_DIR}/PAIR_matched_R2.fastq.gz \
+                            --output_table ${OUT_DIR}/PAIR_matched_extracted_data_table.txt
+
+
+
